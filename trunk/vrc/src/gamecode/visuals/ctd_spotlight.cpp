9f75a90011f684238fb59d11521c639311a9c1bd
botorabi <botorabi@789ac5a4-3de6-0310-9700-cb8f6bc1df3f>
2005-04-16 14:59:00 +0000

    spot light entity

---
+++ trunk/vrc/src/gamecode/visuals/ctd_spotlight.cpp
@@ -0,0 +1,125 @@
+/****************************************************************
+ *  3D Game 'Capture The Diamond'
+ *  Copyright (C) 2004-2006, Ali Botorabi
+ *
+ *  This program is free software; you can redistribute it and/or 
+ *  modify it under the terms of the GNU General Public License 
+ *  as published by the Free Software Foundation; either version 2
+ *  of the License, or (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public 
+ *  License along with this program; if not, write to the Free 
+ *  Software Foundation, Inc., 59 Temple Place, Suite 330, Boston, 
+ *  MA  02111-1307  USA
+ * 
+ ****************************************************************/
+
+/*###############################################################
+ # entity pointlight
+ #
+ #   date of creation:  04/14/2005
+ #
+ #   author:            ali botorabi (boto) 
+ #      e-mail:         botorabi@gmx.net
+ #
+ ################################################################*/
+
+#include <ctd_base.h>
+#include <ctd_application.h>
+#include <ctd_levelmanager.h>
+#include <ctd_log.h>
+#include "ctd_spotlight.h"
+
+using namespace std;
+
+namespace CTD
+{
+//! Implement and register the spotlight entity factory
+CTD_IMPL_ENTITYFACTORY_AUTO( SpotLightEntityFactory );
+
+EnSpotLight::EnSpotLight() :
+_maxDistance( 10.0f ),
+_direction( osg::Vec3f( 0, 0, -1.0f ) ),
+_spotCutOff( 50.0 ),
+_spotExponent( 32.0f )
+{
+    // register entity attributes
+    _attributeManager.addAttribute( "position"             , _position             );
+    _attributeManager.addAttribute( "maxDistance"          , _maxDistance          );
+    _attributeManager.addAttribute( "meshFile"             , _meshFile             );
+    _attributeManager.addAttribute( "ambientColor"         , _ambientColor         );
+    _attributeManager.addAttribute( "diffuseColor"         , _diffuseColor         );
+    _attributeManager.addAttribute( "specularColor"        , _specularColor        );
+    _attributeManager.addAttribute( "constantAttenuation"  , _constAttenuation     );
+    _attributeManager.addAttribute( "linearAttenuation"    , _linearAttenuation    );
+    _attributeManager.addAttribute( "quadraticAttenuation" , _quadraticAttenuation );
+    _attributeManager.addAttribute( "direction"            , _direction            );
+    _attributeManager.addAttribute( "spotCutOff"           , _spotCutOff           );
+    _attributeManager.addAttribute( "spotExponent"         , _spotExponent         );
+}
+
+EnSpotLight::~EnSpotLight()
+{
+}
+
+void EnSpotLight::initialize()
+{
+    // call the get method of light manager so it can register itself for getting camera callbacks (see constructor)
+    LightManager::get();
+
+    // create a new light
+    _lightSource = new osg::LightSource;
+    osg::Light* p_light = new osg::Light;
+    _lightSource->setLight( p_light );
+    _lightSource->setLocalStateSetModes( osg::StateAttribute::ON );
+    // we do culling ourselves
+    _lightSource->setCullingActive( false );
+
+    // set cull callback. it manages the enabling / disabling of lights depending on camera's 
+    //  frustum and light radius.
+    osg::ref_ptr< LightCallback > cullcallback = new LightCallback( this );
+    _lightSource->setCullCallback( cullcallback.get() );
+
+    // setup bounding sphere used for culling
+    osg::Vec3f len = _direction;
+    len.normalize();
+    len *= _maxDistance;
+    osg::Vec3f center( _position + len * 0.5f );
+    _bSphere.set( center, _maxDistance * 0.5f );
+    // the id will be set by light manager
+    p_light->setLightNum( _lightId );
+
+    p_light->setPosition( osg::Vec4( 0, 0, 0, 1.0f ) );
+    p_light->setDirection( _direction );
+    p_light->setSpotCutoff( _spotCutOff );
+    p_light->setSpotExponent( _spotExponent );
+    p_light->setAmbient( osg::Vec4( _ambientColor, 1.0f ) );
+    p_light->setDiffuse( osg::Vec4( _diffuseColor, 1.0f ) );
+    p_light->setSpecular( osg::Vec4( _specularColor, 1.0f ) );
+    p_light->setConstantAttenuation( _constAttenuation );
+    p_light->setLinearAttenuation( _linearAttenuation );
+    p_light->setQuadraticAttenuation( _quadraticAttenuation );
+
+    // add light to entity's transform node
+    addToTransformableNode( _lightSource.get() );
+       
+    // set mesh if one defined
+    if ( _meshFile.length() )
+    {
+        osg::ref_ptr< osg::Node > mesh = LevelManager::get()->loadMesh( _meshFile );
+        if ( !mesh.valid() ) 
+            log << Log::LogLevel( Log::L_WARNING ) << " cannot find mesh file" << _meshFile << endl;
+        else
+            addToTransformableNode( mesh.get() );
+    }
+    
+    // set entity position which is also the light source position
+    setPosition( _position );
+}
+
+} // namespace CTD
---
+++ trunk/vrc/src/gamecode/visuals/ctd_spotlight.h
@@ -0,0 +1,83 @@
+/****************************************************************
+ *  3D Game 'Capture The Diamond'
+ *  Copyright (C) 2004-2006, Ali Botorabi
+ *
+ *  This program is free software; you can redistribute it and/or 
+ *  modify it under the terms of the GNU General Public License 
+ *  as published by the Free Software Foundation; either version 2
+ *  of the License, or (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public 
+ *  License along with this program; if not, write to the Free 
+ *  Software Foundation, Inc., 59 Temple Place, Suite 330, Boston, 
+ *  MA  02111-1307  USA
+ * 
+ ****************************************************************/
+
+/*###############################################################
+ # entity spotlight
+ #
+ #   date of creation:  04/16/2005
+ #
+ #   author:            ali botorabi (boto) 
+ #      e-mail:         botorabi@gmx.net
+ #
+ ################################################################*/
+
+#ifndef _CTD_SPOTLIGHT_H_
+#define _CTD_SPOTLIGHT_H_
+
+#include <ctd_base.h>
+#include <ctd_baseentity.h>
+#include <ctd_entitymanager.h>
+#include "ctd_lightmanager.h"
+
+namespace CTD
+{
+
+#define ENTITY_NAME_SPOTLIGHT    "SpotLight"
+
+//! Spot light entity
+class EnSpotLight :  public BaseEntity, public BaseLight
+{
+    public:
+                                                    EnSpotLight();
+
+        virtual                                     ~EnSpotLight();
+
+        /**
+        * Initializing function, this is called after all engine modules are initialized and a map is loaded.
+        */
+        void                                        initialize();
+
+    protected:
+
+        osg::Vec3f                                  _direction;
+
+        float                                       _spotCutOff;
+
+        float                                       _spotExponent;
+
+        //! Max distance used for culling ( deactivating ) light source during rendering
+        float                                       _maxDistance;
+};
+
+//! Entity type definition used for type registry
+class SpotLightEntityFactory : public BaseEntityFactory
+{
+    public:
+                                                    SpotLightEntityFactory() : BaseEntityFactory( ENTITY_NAME_SPOTLIGHT ) {}
+
+        virtual                                     ~SpotLightEntityFactory() {}
+
+        Macro_CreateEntity( EnSpotLight );
+};
+
+}
+
+#endif // _CTD_SPOTLIGHT_H_
