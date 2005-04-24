4d820e386a00465063b59cb659226072de4f3ae5
botorabi <botorabi@789ac5a4-3de6-0310-9700-cb8f6bc1df3f>
2005-04-24 18:55:36 +0000

    complete menu system is handled by this entity. work in progress ...

---
+++ trunk/vrc/src/gamecode/gui/cd_menu.cpp
@@ -0,0 +1,71 @@
+/****************************************************************
+ *  3D Game 'Capture The Diamond'
+ *  Copyright (C) 2004-2006, Ali Botorabi
+ *
+ *  This program is free software; you can redistribute it and/or 
+ *  modify it under the terms of the GNU General Public License 
+ *  as published by the Free Software Foundation; either version 2
+ *  of the License, or (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public 
+ *  License along with this program; if not, write to the Free 
+ *  Software Foundation, Inc., 59 Temple Place, Suite 330, Boston, 
+ *  MA  02111-1307  USA
+ * 
+ ****************************************************************/
+
+/*###############################################################
+ # entity game menu
+ #
+ #   date of creation:  04/24/2005
+ #
+ #   author:            ali botorabi (boto) 
+ #      e-mail:         botorabi@gmx.net
+ #
+ ################################################################*/
+
+#include <ctd_base.h>
+#include <ctd_application.h>
+#include <ctd_guimsgbox.h>
+#include <ctd_log.h>
+#include "ctd_menu.h"
+#include "ctd_dialogsettings.h"
+
+using namespace std;
+using namespace CTD; 
+
+//! Implement and register the menu entity factory
+CTD_IMPL_ENTITYFACTORY_AUTO( MenuEntityFactory );
+
+
+EnMenu::EnMenu()
+{
+    EntityManager::get()->registerUpdate( this );   // register entity in order to get updated per simulation step
+
+    // register entity attributes
+    _attributeManager.addAttribute( "menuConfig"              , _menuConfig           );
+    _attributeManager.addAttribute( "settingsDialogConfig"    , _settingsDialogConfig );
+}
+
+EnMenu::~EnMenu()
+{
+}
+
+void EnMenu::initialize()
+{
+    // setup dialog for editing game settings
+    _settingsDialog = auto_ptr< DialogGameSettings >( new DialogGameSettings );
+    if ( !_settingsDialog->initialize( _settingsDialogConfig ) )
+        return;
+    
+    _settingsDialog->show( true );
+}
+
+void EnMenu::updateEntity( float deltaTime )
+{
+}
---
+++ trunk/vrc/src/gamecode/gui/ctd_menu.h
@@ -0,0 +1,94 @@
+/****************************************************************
+ *  3D Game 'Capture The Diamond'
+ *  Copyright (C) 2004-2006, Ali Botorabi
+ *
+ *  This program is free software; you can redistribute it and/or 
+ *  modify it under the terms of the GNU General Public License 
+ *  as published by the Free Software Foundation; either version 2
+ *  of the License, or (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public 
+ *  License along with this program; if not, write to the Free 
+ *  Software Foundation, Inc., 59 Temple Place, Suite 330, Boston, 
+ *  MA  02111-1307  USA
+ * 
+ ****************************************************************/
+
+/*###############################################################
+ # entity game menu
+ #
+ #   date of creation:  04/24/2005
+ #
+ #   author:            ali botorabi (boto) 
+ #      e-mail:         botorabi@gmx.net
+ #
+ ################################################################*/
+
+#ifndef _CTD_MENU_H_
+#define _CTD_MENU_H_
+
+#include <ctd_base.h>
+#include <ctd_baseentity.h>
+#include <ctd_entitymanager.h>
+
+namespace CTD
+{
+
+#define ENTITY_NAME_MENU    "Menu"
+
+class DialogGameSettings;
+
+//! The menu system is controlled by this entity
+class EnMenu :  public BaseEntity
+{
+    public:
+                                                    EnMenu();
+
+        virtual                                     ~EnMenu();
+
+
+        //! This entity does not need a transform node, which would be created by level manager on loading
+        //!   We create an own one and add it into scene's root node
+        bool                                        needTransformation() { return false; }
+
+        /**
+        * Initializing function, this is called after all engine modules are initialized and a map is loaded.
+        */
+        void                                        initialize();
+
+        //! This entity needs updating
+        void                                        updateEntity( float deltaTime );
+
+    protected:
+
+        //! Entity parameters
+
+        std::string                                 _menuConfig;
+
+        std::string                                 _settingsDialogConfig;
+
+    protected:
+
+        std::auto_ptr< DialogGameSettings >         _settingsDialog;
+
+};
+
+//! Entity type definition used for type registry
+class MenuEntityFactory : public BaseEntityFactory
+{
+    public:
+                                                    MenuEntityFactory() : BaseEntityFactory( ENTITY_NAME_MENU ) {}
+
+        virtual                                     ~MenuEntityFactory() {}
+
+        Macro_CreateEntity( EnMenu );
+};
+
+}
+
+#endif // _CTD_MENU_H_
