c2edc7e66e8151fdac77a609755183a7a4e8a1a0
botorabi <botorabi@789ac5a4-3de6-0310-9700-cb8f6bc1df3f>
2005-04-27 09:33:05 +0000

    separation of framework and game code

---
+++ trunk/vrc/src/gamecode/ctd_main.h
@@ -0,0 +1,54 @@
+/****************************************************************
+ *  3D Game 'Capture The Diamond'
+ *  Copyright (C) 2004-2006, Ali Botorabi
+ *
+ *  This program is free software; you can redistribute it and/or 
+ *  modify it under the terms of the GNU General Public License 
+ *  as published by the Free Software Foundation; either version 2
+ *  of the License, or (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public 
+ *  License along with this program; if not, write to the Free 
+ *  Software Foundation, Inc., 59 Temple Place, Suite 330, Boston, 
+ *  MA  02111-1307  USA
+ * 
+ ****************************************************************/
+
+/*###############################################################
+ # source file for building precompiled headers
+ #
+ #   date of creation:  04/27/2005
+ #
+ #   author:            ali botorabi (boto) 
+ #      e-mail:         botorabi@gmx.net
+ #
+ #
+ ################################################################*/
+
+
+#ifndef _CTD_MAIN_H_
+#define _CTD_MAIN_H_
+
+// ms windows stuff
+#ifdef WIN32
+#   define CRTDBG_MAP_ALLOC
+#   include <stdlib.h>
+#   include <crtdbg.h>
+#  ifndef WIN32_LEAN_AND_MEAN
+#    define WIN32_LEAN_AND_MEAN
+#  endif
+#  include <windows.h>
+#endif
+
+// framework's central header for most important dependencies
+#include <ctd_base.h>
+// main header of framework itself
+#include <ctd_framework.h>
+
+
+#endif // _CTD_MAIN_H_
