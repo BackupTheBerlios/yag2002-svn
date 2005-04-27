9ab9848eb111abcf44c99f6e29316b7859cf876b
botorabi <botorabi@789ac5a4-3de6-0310-9700-cb8f6bc1df3f>
2005-04-27 09:31:31 +0000

    convenient main header for framework which can be used by game code

---
+++ trunk/vrc/src/framework/ctd_framework.h
@@ -0,0 +1,54 @@
+/****************************************************************
+ *  3D Game 'Capture The Diamond'
+ *  Copyright (C) 2004-2006, Ali Botorabi
+ *
+ *  This program is free software; you can redistribute it and/or 
+ *  modify it under the terms of the GNU General Public License 
+ *  as published by the Free Software Foundation; either version 2
+ *  of the License, or (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public 
+ *  License along with this program; if not, write to the Free 
+ *  Software Foundation, Inc., 59 Temple Place, Suite 330, Boston, 
+ *  MA  02111-1307  USA
+ * 
+ ****************************************************************/
+
+/*###############################################################
+ # framework's main header
+ #
+ #   date of creation:  04/27/2005
+ #
+ #   author:            ali botorabi (boto) 
+ #      e-mail:         botorabi@gmx.net
+ #
+ #
+ ################################################################*/
+
+
+#ifndef _CTD_FRAMEWORK_H_
+#define _CTD_FRAMEWORK_H_
+
+#include <ctd_application.h>
+#include <ctd_attributemanager.h>
+#include <ctd_baseentity.h>
+#include <ctd_configuration.h>
+#include <ctd_entitymanager.h>
+#include <ctd_gamestate.h>
+#include <ctd_guimanager.h>
+#include <ctd_guimsgbox.h>
+#include <ctd_keymap.h>
+#include <ctd_levelmanager.h>
+#include <ctd_log.h>
+#include <ctd_network.h>
+#include <ctd_physics.h>
+#include <ctd_settings.h>
+#include <ctd_singleton.h>
+#include <ctd_utils.h>
+
+#endif // _CTD_FRAMEWORK_H_
