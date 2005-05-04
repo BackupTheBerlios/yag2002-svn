67961df36c4c557816a37fb4cde57f1f7b175513
botorabi <botorabi@789ac5a4-3de6-0310-9700-cb8f6bc1df3f>
2005-05-04 18:38:29 +0000

    fog entity

---
+++ trunk/ctd/src/gamecode/visuals/ctd_fog.cpp
@@ -0,0 +1,79 @@
+/****************************************************************
+ *  YAG2002 (http://yag2002.sourceforge.net)
+ *  Copyright (C) 2005-2007, A. Botorabi
+ *
+ *  This program is free software; you can redistribute it and/or 
+ *  modify it under the terms of the GNU Lesser General Public 
+ *  License version 2.1 as published by the Free Software Foundation.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public 
+ *  License along with this program; if not, write to the Free 
+ *  Software Foundation, Inc., 59 Temple Place, Suite 330, Boston, 
+ *  MA  02111-1307  USA
+ * 
+ ****************************************************************/
+
+/*###############################################################
+ # entity for fog setup
+ #
+ #   date of creation:  04/05/2005
+ #
+ #   author:            ali botorabi (boto) 
+ #      e-mail:         botorabi@gmx.net
+ #
+ ################################################################*/
+
+#include <ctd_main.h>
+#include "ctd_fog.h"
+
+namespace CTD
+{
+
+//! Implement and register the fog entity factory
+CTD_IMPL_ENTITYFACTORY_AUTO( FogEntityFactory );
+
+EnFog::EnFog() :
+_density( 0.3f ),
+_start( 300.0f ),
+_end( 1000.0f ),
+_fogColor( osg::Vec3f( 0.2f, 0.2f, 0.2f ) ),
+_p_fog( NULL )
+{
+    // register entity attributes
+    _attributeManager.addAttribute( "density"   , _density   );
+    _attributeManager.addAttribute( "start"     , _start     );
+    _attributeManager.addAttribute( "end"       , _end       );
+    _attributeManager.addAttribute( "color"     , _fogColor  );
+}
+
+EnFog::~EnFog()
+{
+    if ( _p_fog )
+    {
+        osgProducer::Viewer* p_viewer = Application::get()->getViewer();
+        osg::StateSet* p_stateset = p_viewer->getGlobalStateSet();
+        p_stateset->removeAttribute( _p_fog );
+    }
+}
+
+void EnFog::initialize()
+{
+    osgProducer::Viewer* p_viewer = Application::get()->getViewer();
+    osg::StateSet* p_stateset = p_viewer->getGlobalStateSet();
+    _p_fog = new osg::Fog;
+    _p_fog->setMode( osg::Fog::LINEAR );
+    _p_fog->setDensity( _density );
+    _p_fog->setStart( _start );
+    _p_fog->setEnd( _end );
+    _p_fog->setColor( osg::Vec4f( _fogColor, 1.0f ) );
+
+    p_stateset->setAttributeAndModes( _p_fog, osg::StateAttribute::ON );
+    p_stateset->setMode( GL_FOG, osg::StateAttribute::ON );
+}
+
+} // namespace CTD
---
+++ trunk/ctd/src/gamecode/visuals/ctd_fog.h
@@ -0,0 +1,81 @@
+/****************************************************************
+ *  YAG2002 (http://yag2002.sourceforge.net)
+ *  Copyright (C) 2005-2007, A. Botorabi
+ *
+ *  This program is free software; you can redistribute it and/or 
+ *  modify it under the terms of the GNU Lesser General Public 
+ *  License version 2.1 as published by the Free Software Foundation.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public 
+ *  License along with this program; if not, write to the Free 
+ *  Software Foundation, Inc., 59 Temple Place, Suite 330, Boston, 
+ *  MA  02111-1307  USA
+ * 
+ ****************************************************************/
+
+/*###############################################################
+ # entity for fog setup
+ #
+ #   date of creation:  04/05/2005
+ #
+ #   author:            ali botorabi (boto) 
+ #      e-mail:         botorabi@gmx.net
+ #
+ ################################################################*/
+
+#ifndef _CTD_FOG_H_
+#define _CTD_FOG_H_
+
+#include <ctd_main.h>
+
+namespace CTD
+{
+
+#define ENTITY_NAME_FOG    "Fog"
+
+//! This entity sets up fog.
+class EnFog :  public BaseEntity
+{
+    public:
+                                                    EnFog();
+
+        virtual                                     ~EnFog();
+
+        //! Initializing function, this is called after all engine modules are initialized and a map is loaded.
+        void                                        initialize();
+
+    protected:
+
+        float                                       _density;
+
+        float                                       _start;
+
+        float                                       _end;
+
+        //! Fog color
+        osg::Vec3f                                  _fogColor;
+
+    protected:
+
+        osg::Fog*                                   _p_fog;
+};
+
+//! Entity type definition used for type registry
+class FogEntityFactory : public BaseEntityFactory
+{
+    public:
+                                                    FogEntityFactory() : BaseEntityFactory( ENTITY_NAME_FOG ) {}
+
+        virtual                                     ~FogEntityFactory() {}
+
+        Macro_CreateEntity( EnFog );
+};
+
+} // namespace CTD
+
+#endif // _CTD_FOG_H_
