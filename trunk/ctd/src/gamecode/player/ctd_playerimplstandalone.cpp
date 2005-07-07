9fdc8ee5fba8fcd81dc2b4d14a0399900a6ae34a
botorabi <botorabi@789ac5a4-3de6-0310-9700-cb8f6bc1df3f>
2005-07-07 18:59:46 +0000

    improved the initialization process

--- trunk/ctd/src/gamecode/player/ctd_playerimplstandalone.cpp
+++ trunk/ctd/src/gamecode/player/ctd_playerimplstandalone.cpp
@@ -47,9 +47,6 @@ PlayerImplStandalone::PlayerImplStandalone( EnPlayer* player ) :
 BasePlayerImplementation( player ),
 _p_inputHandler( NULL )
 {
-    // create a new input handler for this player
-    _p_inputHandler = new PlayerIHCharacterCameraCtrl< PlayerImplStandalone >( this, player );
-    _p_chatGui = std::auto_ptr< PlayerChatGui >( new PlayerChatGui );
 }
 
 PlayerImplStandalone::~PlayerImplStandalone()
@@ -66,29 +63,37 @@ void PlayerImplStandalone::handleNotification( const EntityNotification& notific
         case CTD_NOTIFY_MENU_ENTER:
 
             _p_chatGui->show( false );
-            _p_inputHandler->setMenuEnabled( true );
+            if ( _p_inputHandler )
+                _p_inputHandler->setMenuEnabled( true );
             
             // reset player's movement and sound
-            _p_playerPhysics->stopMovement();
-            _p_playerAnimation->animIdle();
+            if ( _p_playerPhysics )
+                _p_playerPhysics->stopMovement();
+            if ( _p_playerAnimation )
+                _p_playerAnimation->animIdle();
             if ( _p_playerSound )
                 _p_playerSound->stopPlayingAll();
 
             // very important: diable the camera when we enter menu!
-            _p_camera->setEnable( false );
+            if ( _p_camera )
+                _p_camera->setEnable( false );
 
             break;
 
         case CTD_NOTIFY_MENU_LEAVE:
         {
             _p_chatGui->show( true );
-            _p_inputHandler->setMenuEnabled( false );
 
-            // refresh our configuration settings
-            getConfiguration();
+            if ( _p_inputHandler )
+            {
+                _p_inputHandler->setMenuEnabled( false );
+                // refresh our configuration settings
+                getConfiguration();
+            }
  
             // very important: enable the camera when we leave menu!
-            _p_camera->setEnable( true );
+            if ( _p_camera )
+                _p_camera->setEnable( true );
         }
         break;
 
@@ -103,9 +108,6 @@ void PlayerImplStandalone::initialize()
     _currentPos = getPlayerEntity()->getPosition();
     _currentRot = getPlayerEntity()->getRotation();
     _moveDir    = _currentRot * _moveDir;
-
-    _p_chatGui->initialize( this, _playerAttributes._chatGuiConfig );
-    _p_inputHandler->setMenuEnabled( false );
 }
 
 void PlayerImplStandalone::postInitialize()
@@ -116,53 +118,82 @@ void PlayerImplStandalone::postInitialize()
     log << Log::LogLevel( Log::L_DEBUG ) << "   - searching for camera entity '" << PLAYER_CAMERA_ENTITIY_NAME << "'..." << endl;
     // get camera entity
     _p_camera = dynamic_cast< EnCamera* >( EntityManager::get()->findEntity( ENTITY_NAME_CAMERA, PLAYER_CAMERA_ENTITIY_NAME ) );
-    assert( _p_camera && "could not find the camera entity!" );
-    log << Log::LogLevel( Log::L_DEBUG ) << "   -  camera entity successfully attached" << endl;
+    if ( _p_camera )
+    {
+        log << Log::LogLevel( Log::L_DEBUG ) << "   -  camera entity successfully attached" << endl;
+    }
+    else
+    {
+        log << Log::LogLevel( Log::L_ERROR ) << "   could not attach player camera entity" << endl;
+    }
 
     // attach physics entity
     log << Log::LogLevel( Log::L_DEBUG ) << "   - searching for physics entity '" << _playerAttributes._physicsEntity << "' ..." << endl;
     // find and attach physics component
     _p_playerPhysics = dynamic_cast< EnPlayerPhysics* >( EntityManager::get()->findEntity( ENTITY_NAME_PLPHYS, _playerAttributes._physicsEntity ) );
-    assert( _p_playerPhysics && "given instance name does not belong to a EnPlayerPhysics entity type!" );
-    _p_playerPhysics->setPlayer( this );
-    log << Log::LogLevel( Log::L_DEBUG ) << "   -  physics entity successfully attached" << endl;
+    if ( _p_playerPhysics )
+    {
+        _p_playerPhysics->setPlayer( this );
+        log << Log::LogLevel( Log::L_DEBUG ) << "   -  physics entity successfully attached" << endl;
+    }
+    else
+    {
+        log << Log::LogLevel( Log::L_ERROR ) << "   could not attach player physics entity" << endl;
+    }
 
     // attach animation entity
     log << Log::LogLevel( Log::L_DEBUG ) << "   - searching for animation entity '" << _playerAttributes._animationEntity << "' ..." << endl;
     // find and attach animation component
     _p_playerAnimation = dynamic_cast< EnPlayerAnimation* >( EntityManager::get()->findEntity( ENTITY_NAME_PLANIM, _playerAttributes._animationEntity ) );
-    assert( _p_playerAnimation && "given instance name does not belong to a EnPlayerAnimation entity type!" );
-    _p_playerAnimation->setPlayer( this );
-    if ( _cameraMode == Ego ) // in ego mode we won't render our character
+    if ( _p_playerAnimation )
     {
-        _p_playerAnimation->enableRendering( false );
-    }
-    else // if in spheric mode disable the mouse pointer
-    {
-        GuiManager::get()->showMousePointer( false );
-    }
+        _p_playerAnimation->setPlayer( this );
+        log << Log::LogLevel( Log::L_DEBUG ) << "   -  animation entity successfully attached" << endl;
 
-    log << Log::LogLevel( Log::L_DEBUG ) << "   -  animation entity successfully attached" << endl;
+        if ( _cameraMode == Ego ) // in ego mode we won't render our character
+        {
+            _p_playerAnimation->enableRendering( false );
+        }
+        else // if in spheric mode disable the mouse pointer
+        {
+            GuiManager::get()->showMousePointer( false );
+        }
+    }
+    else
+    {
+        log << Log::LogLevel( Log::L_ERROR ) << "   could not attach player animation entity" << endl;
+    }
 
     // attach sound entity
     log << Log::LogLevel( Log::L_DEBUG ) << "   - searching for sound entity '" << _playerAttributes._soundEntity << "' ..." << endl;
     // find and attach sound component, tollerate missing sound for now
     _p_playerSound = dynamic_cast< EnPlayerSound* >( EntityManager::get()->findEntity( ENTITY_NAME_PLSOUND, _playerAttributes._soundEntity ) );
     if ( !_p_playerSound )
-        log << Log::LogLevel( Log::L_ERROR ) << "  *** could not find sound entity '" << _playerAttributes._soundEntity << "' of type PlayerSound. player sound deactivated" << endl;
+        log << Log::LogLevel( Log::L_ERROR ) << "   could not find sound entity '" << _playerAttributes._soundEntity << "' of type PlayerSound. player sound deactivated" << endl;
     else
     {
         _p_playerSound->setPlayer( this );
         log << Log::LogLevel( Log::L_DEBUG ) << "   -  sound entity successfully attached" << endl;
     }
 
-    // get configuration settings
-    getConfiguration();
-
     // setup camera mode
     setCameraMode( _cameraMode );
 
     log << Log::LogLevel( Log::L_INFO ) << "  player implementation successfully initialized" << endl;
+
+    _p_chatGui = std::auto_ptr< PlayerChatGui >( new PlayerChatGui );
+    _p_chatGui->initialize( this, _playerAttributes._chatGuiConfig );
+
+    // create only the input handler when animation and physics are attached
+    if ( _p_playerAnimation && _p_playerPhysics )
+    {
+        // create a new input handler for this player
+        _p_inputHandler = new PlayerIHCharacterCameraCtrl< PlayerImplStandalone >( this, getPlayerEntity() );
+        _p_inputHandler->setMenuEnabled( false );
+
+        // get configuration settings
+        getConfiguration();
+    }
 }
 
 void PlayerImplStandalone::getConfiguration()
@@ -204,7 +235,8 @@ void PlayerImplStandalone::update( float deltaTime )
     getPlayerEntity()->setRotation( _currentRot ); 
 
     // adjust the camera to updated position and rotation. the physics updates the translation of player.
-    _p_camera->setCameraTranslation( getPlayerPosition(), getPlayerRotation() );
+    if ( _p_camera )
+        _p_camera->setCameraTranslation( getPlayerPosition(), getPlayerRotation() );
 
     _p_chatGui->update( deltaTime );
 }
