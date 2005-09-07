72d37f89e612ae6f05d83a74a7b19910e76ae627
botorabi <botorabi@789ac5a4-3de6-0310-9700-cb8f6bc1df3f>
2005-09-07 17:43:53 +0000

    ported directory listing and time related functions, work still in progress

--- trunk/ctd/src/framework/ctd_utils.cpp
+++ trunk/ctd/src/framework/ctd_utils.cpp
@@ -37,24 +37,26 @@
 #ifdef WIN32
  #include <shellapi.h>
 #endif
+#ifdef LINUX
+ #include <sys/types.h>
+ #include <sys/stat.h>
+ #include <glob.h>
+ #include <dirent.h>
+#endif
 
 namespace CTD
 {
 
 std::string getTimeStamp()
 {
-#ifdef WIN32	
-
+#ifdef WIN32
     __time64_t ltime;
     _time64( &ltime );
-    return std::string( _ctime64( &ltime ) );
-	
+    return std::string( _ctime64( &ltime ) );   
 #endif
 #ifdef LINUX
- 
-	// TODO: implemenation
-	return std::string( "not implemented yet" );
-	
+    time_t ltime;
+    return std::string( ctime( &ltime ) );
 #endif
 
     return std::string( "platform not supported" );
@@ -62,30 +64,30 @@ std::string getTimeStamp()
 
 std::string::size_type explode( const std::string& str, const std::string& separators, std::vector< std::string >* p_result )
 {
-	std::string::size_type len = str.length();
-	if( !len )
-		return 0;
+    std::string::size_type len = str.length();
+    if( !len )
+        return 0;
 
-	if( !separators.length() )
-	{
-		p_result->push_back( str );
-		return 1;
-	}
+    if( !separators.length() )
+    {
+        p_result->push_back( str );
+        return 1;
+    }
 
-	std::string::size_type token = 0;
-	std::string::size_type end   = 0;
-	unsigned int      org   = p_result->size();
+    std::string::size_type token = 0;
+    std::string::size_type end   = 0;
+    unsigned int      org   = p_result->size();
 
-	while( end < len )
-	{
-		token = str.find_first_not_of( separators, end );
-		end   = str.find_first_of( separators, token );
+    while( end < len )
+    {
+        token = str.find_first_not_of( separators, end );
+        end   = str.find_first_of( separators, token );
 
-		if( token != std::string::npos )
-			p_result->push_back( str.substr( token, ( end != std::string::npos ) ? ( end - token ) : std::string::npos ) );
-	}
+        if( token != std::string::npos )
+            p_result->push_back( str.substr( token, ( end != std::string::npos ) ? ( end - token ) : std::string::npos ) );
+    }
 
-	return( p_result->size() - org );
+    return( p_result->size() - org );
 }
 
 std::string extractPath( const std::string& fullpath )
@@ -122,45 +124,190 @@ std::string cleanPath( const std::string& path )
     return cleanpath;
 }
 
-osg::ref_ptr< osg::TextureCubeMap > readCubeMap( const std::vector< std::string >& texfiles )
+std::string getCurrentWorkingDirectory()
 {
-    if ( texfiles.size() < 6 )
+#ifdef WIN32
+
+    char buf[ 512 ];
+    GetCurrentDirectory( sizeof( buf ), buf );
+    return std::string( buf );
+
+#endif
+#ifdef LINUX
+    
+    char buf[ 512 ];
+    char* p_res = getcwd( buf, sizeof( buf ) );
+    assert( p_res && "error getting current working directory" );
+
+#endif
+
+   return std::string( buf );
+}
+
+#ifdef WIN32
+std::string convertFileTime( const FILETIME& ft )
+{
+    SYSTEMTIME systemTime;
+    SYSTEMTIME localTime;
+    // convert the time structure to system time
+    if ( !FileTimeToSystemTime( &ft, &systemTime ) )
+        return "";
+    // now convert the system time to local time
+    if ( !SystemTimeToTzSpecificLocalTime( NULL, &systemTime, &localTime ) )
+        return "";
+
+    // format the time string
+    char buf[ 128 ];
+    sprintf( buf, "%0.2d:%0.2d:%0.4d  %0.2d:%0.2d", localTime.wMonth, localTime.wDay, localTime.wYear, localTime.wHour, localTime.wMinute );
+    return std::string( buf );
+}
+#endif
+
+void getDirectoryListing( std::vector< std::string >& listing, const std::string& dir, const std::string& extension, bool appenddetails )
+{
+    std::list< std::pair< std::string, std::string > > files;
+
+#ifdef WIN32
+    WIN32_FIND_DATA findData;
+    HANDLE          fileHandle;
+    int             flag = 1;
+    std::string     search ( ( extension == "" ) ? "*" : "*." );
+    std::string     directory( dir );
+
+    if ( directory == "" )
+        directory = ".";
+
+    directory += "/";
+    search = directory + search + extension;
+    fileHandle = FindFirstFile( search.c_str(), &findData );
+    if ( fileHandle == INVALID_HANDLE_VALUE )
+        return;
+
+    while ( flag )
     {
-        log << Log::LogLevel( Log::L_ERROR ) << "readCubeMap: 6 tex files are needed, but only " << texfiles.size() << " given!" << std::endl;
-        return NULL;
+        if ( strcmp( findData.cFileName, "." ) && strcmp( findData.cFileName, ".." ) )
+        {
+            std::string details;
+            if ( appenddetails )
+                details = convertFileTime( findData.ftLastWriteTime );
+
+            // check for directories
+            if( findData.dwFileAttributes & FILE_ATTRIBUTE_DIRECTORY )
+                files.push_back( make_pair( "[" + std::string( findData.cFileName ) + "]", details ) );
+            else
+                files.push_back( make_pair( findData.cFileName, details ) );
+        }
+        flag = FindNextFile( fileHandle, &findData );
     }
+    FindClose( fileHandle );
+#endif
+#ifdef LINUX
+    struct stat     fileInfo;
+    glob_t          ff_glob;
+    std::string     directory( dir );
 
-    osg::TextureCubeMap* p_cubemap = new osg::TextureCubeMap;
-    std::string mediapath = Application::get()->getMediaPath();
-    osg::Image* imagePosX = osgDB::readImageFile( mediapath + texfiles[ 0 ] );
-    osg::Image* imageNegX = osgDB::readImageFile( mediapath + texfiles[ 1 ] );
-    osg::Image* imagePosY = osgDB::readImageFile( mediapath + texfiles[ 2 ] );
-    osg::Image* imageNegY = osgDB::readImageFile( mediapath + texfiles[ 3 ] );
-    osg::Image* imagePosZ = osgDB::readImageFile( mediapath + texfiles[ 4 ] );
-    osg::Image* imageNegZ = osgDB::readImageFile( mediapath + texfiles[ 5 ] );
+    if ( directory == "" )
+        directory = ".";
 
-    if ( imagePosX && imageNegX && imagePosY && imageNegY && imagePosZ && imageNegZ )
+    directory += "/";
+    std::string search = directory + "*" + extension; 
+
+    // check if the directory exists  
+    if ( opendir( directory.c_str() ) == NULL )
+        return;
+
+    glob( search.c_str(), 0, NULL, &ff_glob );
+    for ( size_t cnt = 0; cnt < ff_glob.gl_pathc; cnt++ )
     {
-        p_cubemap->setImage( osg::TextureCubeMap::POSITIVE_X, imagePosX );
-        p_cubemap->setImage( osg::TextureCubeMap::NEGATIVE_X, imageNegX );
-        p_cubemap->setImage( osg::TextureCubeMap::POSITIVE_Y, imagePosY );
-        p_cubemap->setImage( osg::TextureCubeMap::NEGATIVE_Y, imageNegY );
-        p_cubemap->setImage( osg::TextureCubeMap::POSITIVE_Z, imagePosZ );
-        p_cubemap->setImage( osg::TextureCubeMap::NEGATIVE_Z, imageNegZ );
+        std::string filename( ff_glob.gl_pathv[ cnt ] );
+        filename.erase( 0, directory.size() ); 
+        std::string details;
+        // check for directories
+        stat( std::string( directory + filename ).c_str(), &fileInfo );
 
-        p_cubemap->setWrap( osg::Texture::WRAP_S, osg::Texture::CLAMP_TO_EDGE );
-        p_cubemap->setWrap( osg::Texture::WRAP_T, osg::Texture::CLAMP_TO_EDGE );
-        p_cubemap->setWrap( osg::Texture::WRAP_R, osg::Texture::CLAMP_TO_EDGE );
+        if ( appenddetails )
+        {
+            time_t modtime = fileInfo.st_mtime;
+            details = ctime( &modtime );
+            details.erase( details.size() - 1 ); // remove line break at end of string
+        }
 
-        p_cubemap->setFilter( osg::Texture::MIN_FILTER, osg::Texture::LINEAR_MIPMAP_LINEAR );
-        p_cubemap->setFilter( osg::Texture::MAG_FILTER, osg::Texture::LINEAR );
+        if( S_ISDIR( fileInfo.st_mode ) )
+            files.push_back( make_pair( "[" + std::string( filename ) + "]", details ) );
+        else
+            files.push_back( make_pair( filename, details ) );
     }
-    else
+#endif
+
+    // sort and copy files into listing
+    files.sort();
+    std::list< std::pair< std::string, std::string > >::iterator p_beg = files.begin(), p_end = files.end();
+    for ( ; p_beg != p_end; p_beg++ )
     {
-        log << Log::LogLevel( Log::L_ERROR ) << "readCubeMap: could not setup all cubemap images" << std::endl;
+        if ( appenddetails )
+            listing.push_back( p_beg->second + "   " + p_beg->first );
+        else
+            listing.push_back( p_beg->first );
     }
+}
 
-    return p_cubemap;
+bool checkDirectory( const std::string& dir )
+{
+#ifdef WIN32
+    
+    WIN32_FIND_DATA findData;
+    HANDLE          fileHandle;
+    std::string     cdir = dir;
+    while ( cdir[ cdir.size() - 1 ] == '/' ) cdir.erase( cdir.size() - 1 ); // cut slashes at end
+    fileHandle = FindFirstFile( cdir.c_str(), &findData );
+    if ( ( fileHandle == INVALID_HANDLE_VALUE ) || !( findData.dwFileAttributes & FILE_ATTRIBUTE_DIRECTORY ) )
+    {
+        FindClose( fileHandle );
+        return false;
+    }
+    FindClose( fileHandle );
+    return true;
+    
+#endif
+#ifdef LINUX
+
+    struct stat     fileInfo;
+    return ( stat( dir.c_str(), &fileInfo ) == NULL ) ? false : true;
+
+#endif
+
+    // platform not supported
+    return false;
+}
+
+SPAWN_PROC_ID spawnApplication( const std::string& cmd, const std::string& params )
+{
+#ifdef WIN32    
+
+    HANDLE hProc = NULL;
+    SHELLEXECUTEINFO shellInfo;
+    ::ZeroMemory( &shellInfo, sizeof( shellInfo ) );
+    shellInfo.cbSize = sizeof( shellInfo );
+    shellInfo.fMask = SEE_MASK_NOCLOSEPROCESS;
+    shellInfo.lpFile = cmd.c_str();
+    shellInfo.lpParameters = params.c_str();
+    shellInfo.nShow = SW_SHOWMINIMIZED;
+    if( ::ShellExecuteEx( &shellInfo ) )
+    { 
+        hProc = shellInfo.hProcess;
+    }
+    return hProc;
+    
+#endif
+#ifdef LINUX
+    
+    // TODO: implementation
+    return -1;
+    
+#endif
+
+    // platform not supported
+    return ( SPAWN_PROC_ID )0;
 }
 
 // helper class for enumerateDisplaySettings
@@ -197,27 +344,6 @@ class DispSettings
                                         return false;
                                 }
 };
-
-std::string getCurrentWorkingDirectory()
-{
-#ifdef WIN32
-
-    char buf[ 512 ];
-    GetCurrentDirectory( sizeof( buf ), buf );
-    return std::string( buf );
-
-#endif
-#ifdef LINUX
-	
-    char buf[ 512 ];
-    char* p_res = getcwd( buf, sizeof( buf ) );
-    assert( p_res && "error getting current working directory" );
-
-#endif
-
-   return std::string( buf );
-}
-
 
 void enumerateDisplaySettings( std::vector< std::string >& settings, unsigned int colorbitsfilter )
 {
@@ -293,141 +419,6 @@ void enumerateDisplaySettings( std::vector< std::string >& settings, unsigned in
 }
 
 #ifdef WIN32
-std::string convertFileTime( const FILETIME& ft )
-{
-    SYSTEMTIME systemTime;
-    SYSTEMTIME localTime;
-    // convert the time structure to system time
-    if ( !FileTimeToSystemTime( &ft, &systemTime ) )
-        return "";
-    // now convert the system time to local time
-    if ( !SystemTimeToTzSpecificLocalTime( NULL, &systemTime, &localTime ) )
-        return "";
-
-    // format the time string
-    char buf[ 128 ];
-    sprintf( buf, "%0.2d:%0.2d:%0.4d  %0.2d:%0.2d", localTime.wMonth, localTime.wDay, localTime.wYear, localTime.wHour, localTime.wMinute );
-    return std::string( buf );
-}
-#endif
-
-void getDirectoryListing( std::vector< std::string >& listing, const std::string& dir, const std::string& extension, bool appenddetails )
-{
-#ifdef WIN32
-
-	WIN32_FIND_DATA findData;
-	HANDLE          fileHandle;
-	int             flag = 1;
-	std::string     search ( ( extension == "" ) ? "*" : "*." );
-	std::string     directory( dir );
-	
-    if ( directory == "" ) 
-        directory = ".";
-
-	directory += "/";
-	search = directory + search + extension;
-	fileHandle = FindFirstFile( search.c_str(), &findData );
-	if ( fileHandle == INVALID_HANDLE_VALUE ) 
-        return;
-
-    std::list< std::pair< std::string, std::string > > files;
-	while ( flag )
-	{
-        if ( strcmp( findData.cFileName, "." ) && strcmp( findData.cFileName, ".." ) )
-        {
-            std::string details;
-            if ( appenddetails )
-                details = convertFileTime( findData.ftLastWriteTime );
-
-            // check for directories
-            if( findData.dwFileAttributes & FILE_ATTRIBUTE_DIRECTORY )
-                files.push_back( make_pair( "[" + std::string( findData.cFileName ) + "]", details ) );
-            else
-                files.push_back( make_pair( findData.cFileName, details ) );
-        }
-
-		flag = FindNextFile( fileHandle, &findData );
-	}
-	FindClose( fileHandle );
-
-    // sort and copy files into listing
-    files.sort();
-    std::list< std::pair< std::string, std::string > >::iterator p_beg = files.begin(), p_end = files.end();
-    for ( ; p_beg != p_end; p_beg++ )
-    {
-        if ( appenddetails )
-            listing.push_back( p_beg->second + "   " + p_beg->first );
-        else
-            listing.push_back( p_beg->first );
-    }
-	
-#endif
-#ifdef LINUX
-
-	// stat()
-#endif	
-}
-
-bool checkDirectory( const std::string& dir )
-{
-#ifdef WIN32
-	
-	WIN32_FIND_DATA findData;
-	HANDLE          fileHandle;
-    std::string     cdir = dir;
-    while ( cdir[ cdir.size() - 1 ] == '/' ) cdir.erase( cdir.size() - 1 ); // cut slashes at end
-    fileHandle = FindFirstFile( cdir.c_str(), &findData );
-	if ( ( fileHandle == INVALID_HANDLE_VALUE ) || !( findData.dwFileAttributes & FILE_ATTRIBUTE_DIRECTORY ) )
-    {
-	    FindClose( fileHandle );
-        return false;
-    }
-	FindClose( fileHandle );
-    return true;
-	
-#endif
-#ifdef LINUX
-
-	// TODO: implementation
-    return false;
-
-#endif
-
-    // platform not supported
-    return false;
-}
-
-SPAWN_PROC_ID spawnApplication( const std::string& cmd, const std::string& params )
-{
-#ifdef WIN32	
-
-    HANDLE hProc = NULL;
-    SHELLEXECUTEINFO shellInfo;
-    ::ZeroMemory( &shellInfo, sizeof( shellInfo ) );
-    shellInfo.cbSize = sizeof( shellInfo );
-    shellInfo.fMask = SEE_MASK_NOCLOSEPROCESS;
-    shellInfo.lpFile = cmd.c_str();
-    shellInfo.lpParameters = params.c_str();
-    shellInfo.nShow = SW_SHOWMINIMIZED;
-    if( ::ShellExecuteEx( &shellInfo ) )
-    { 
-        hProc = shellInfo.hProcess;
-    }
-    return hProc;
-	
-#endif
-#ifdef LINUX
-	
-    // TODO: implementation
-    return -1;
-	
-#endif
-
-    // platform not supported
-    return ( SPAWN_PROC_ID )0;
-}
-
-#ifdef WIN32
 #ifdef CTD_ENABLE_HEAPCHECK
 void checkHeap()
 {
@@ -444,5 +435,47 @@ void checkHeap()
 }
 #endif
 #endif
+
+
+osg::ref_ptr< osg::TextureCubeMap > readCubeMap( const std::vector< std::string >& texfiles )
+{
+    if ( texfiles.size() < 6 )
+    {
+        log << Log::LogLevel( Log::L_ERROR ) << "readCubeMap: 6 tex files are needed, but only " << texfiles.size() << " given!" << std::endl;
+        return NULL;
+    }
+
+    osg::TextureCubeMap* p_cubemap = new osg::TextureCubeMap;
+    std::string mediapath = Application::get()->getMediaPath();
+    osg::Image* imagePosX = osgDB::readImageFile( mediapath + texfiles[ 0 ] );
+    osg::Image* imageNegX = osgDB::readImageFile( mediapath + texfiles[ 1 ] );
+    osg::Image* imagePosY = osgDB::readImageFile( mediapath + texfiles[ 2 ] );
+    osg::Image* imageNegY = osgDB::readImageFile( mediapath + texfiles[ 3 ] );
+    osg::Image* imagePosZ = osgDB::readImageFile( mediapath + texfiles[ 4 ] );
+    osg::Image* imageNegZ = osgDB::readImageFile( mediapath + texfiles[ 5 ] );
+
+    if ( imagePosX && imageNegX && imagePosY && imageNegY && imagePosZ && imageNegZ )
+    {
+        p_cubemap->setImage( osg::TextureCubeMap::POSITIVE_X, imagePosX );
+        p_cubemap->setImage( osg::TextureCubeMap::NEGATIVE_X, imageNegX );
+        p_cubemap->setImage( osg::TextureCubeMap::POSITIVE_Y, imagePosY );
+        p_cubemap->setImage( osg::TextureCubeMap::NEGATIVE_Y, imageNegY );
+        p_cubemap->setImage( osg::TextureCubeMap::POSITIVE_Z, imagePosZ );
+        p_cubemap->setImage( osg::TextureCubeMap::NEGATIVE_Z, imageNegZ );
+
+        p_cubemap->setWrap( osg::Texture::WRAP_S, osg::Texture::CLAMP_TO_EDGE );
+        p_cubemap->setWrap( osg::Texture::WRAP_T, osg::Texture::CLAMP_TO_EDGE );
+        p_cubemap->setWrap( osg::Texture::WRAP_R, osg::Texture::CLAMP_TO_EDGE );
+
+        p_cubemap->setFilter( osg::Texture::MIN_FILTER, osg::Texture::LINEAR_MIPMAP_LINEAR );
+        p_cubemap->setFilter( osg::Texture::MAG_FILTER, osg::Texture::LINEAR );
+    }
+    else
+    {
+        log << Log::LogLevel( Log::L_ERROR ) << "readCubeMap: could not setup all cubemap images" << std::endl;
+    }
+
+    return p_cubemap;
+}
 
 } // namespace CTD
