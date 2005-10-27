37c496f5a13d33c8e1c189c69d91a65b8342e91a
botorabi <botorabi@789ac5a4-3de6-0310-9700-cb8f6bc1df3f>
2005-10-27 20:42:38 +0000

    centralized all replicanet realted objects

---
+++ trunk/ctd/src/gamecode/networking/_Def_replicaregs.cpp
@@ -0,0 +1,8 @@
+/* Auto-generated with RNROLCompiler V2.2.0.0 don't edit */
+#include "RNReplicaNet/Inc/ReplicaNet.h"
+#include "RNReplicaNet/Inc/RO_RegistryBlock.h"
+#include "RNReplicaNet/Inc/Define_RO.h"
+_RO_DO_PREREGISTER_FUNC();
+_RO_DEF_DO_PREDEF(ImplChatNetworkingVRC);
+_RO_DEF_DO_PREDEF(PlayerNetworking);
+_RO_DO_PREREGISTER_FUNC_END();
