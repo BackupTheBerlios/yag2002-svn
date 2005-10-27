29d947c2ad17ada9a95b8f59ce90d3d19c66fefb
botorabi <botorabi@789ac5a4-3de6-0310-9700-cb8f6bc1df3f>
2005-10-27 20:37:42 +0000

    centralized file contents

--- trunk/ctd/src/gamecode/player/networkingRoles/_Def_Chat.cpp
+++
@@ -1,8 +0,0 @@
-/* Auto-generated with RNROLCompiler V2.2.0.0 don't edit */
-#include "_Def_Chat.h"
-#include "RNReplicaNet/Inc/ReplicaNet.h"
-#include "RNReplicaNet/Inc/RO_RegistryBlock.h"
-#include "RNReplicaNet/Inc/Define_RO.h"
-_RO_DO_PREREGISTER_FUNC();
-_RO_DEF_DO_PREDEF(PlayerNetworking);
-_RO_DO_PREREGISTER_FUNC_END();
--- trunk/ctd/src/gamecode/player/networkingRoles/_Def_Chat.h
+++
@@ -1,6 +0,0 @@
-/* Auto-generated with RNROLCompiler V2.2.0.0 don't edit */
-#ifndef ___Def_Chat_h__
-#define ___Def_Chat_h__
-#include "RNReplicaNet/Inc/ReplicaObject.h"
-#include "RNReplicaNet/Inc/Define_RO.h"
-#endif
