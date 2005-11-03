498419ae5dd37a7f36d50f3270fd2c45134a9d33
botorabi <botorabi@789ac5a4-3de6-0310-9700-cb8f6bc1df3f>
2005-11-03 00:10:55 +0000

    entity for displaying player entity names

---
+++ trunk/ctd/src/gamecode/player/ctd_playernamedisp.cpp
@@ -0,0 +1,127 @@
+/****************************************************************
+ *  YAG2002 (http://yag2002.sourceforge.net)
+ *  Copyright (C) 2005-2007, A. Botorabi
+ *
+ *  This program is free software; you can redistribute it and/or 
+ *  modify it under the terms of the GNU Lesser General Public 
+ *  License version 2.1 as published by the Free Software Foundation.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public 
+ *  License along with this program; if not, write to the Free 
+ *  Software Foundation, Inc., 59 Temple Place, Suite 330, Boston, 
+ *  MA  02111-1307  USA
+ * 
+ ****************************************************************/
+
+/*###############################################################
+ # entity for displaying name of player in front
+ #
+ #   date of creation:  11/02/2005
+ #
+ #   author:            ali botorabi (boto) 
+ #      e-mail:         botorabi@gmx.net
+ #
+ ################################################################*/
+
+#include <ctd_main.h>
+#include "ctd_playernamedisp.h"
+#include "../ctd_gameutils.h"
+#include "../visuals/ctd_camera.h"
+#include "ctd_playerimpl.h"
+
+namespace CTD
+{
+
+//! Implement and register the entity factory
+CTD_IMPL_ENTITYFACTORY_AUTO( PlayerNameDisplayEntityFactory );
+    
+EnPlayerNameDisplay::EnPlayerNameDisplay() :
+_position( osg::Vec3f( 0.02f, 0.9f ,0.0f ) ),
+_updateTimer( 0.0f )
+{
+    CTD::log << CTD::Log::LogLevel( CTD::Log::L_DEBUG ) << "creating player name display entity"  << std::endl;
+
+    getAttributeManager().addAttribute( "position", _position );
+}
+
+EnPlayerNameDisplay::~EnPlayerNameDisplay()
+{
+    CTD::log << CTD::Log::LogLevel( CTD::Log::L_DEBUG ) << "destroying player name display entity" << std::endl;
+}
+
+void EnPlayerNameDisplay::handleNotification( const EntityNotification& notification )
+{
+    //switch( notification.getId() )
+    //{
+
+    //    default:
+    //        ;
+    //}
+}
+
+void EnPlayerNameDisplay::initialize()
+{
+    // register for getting system notifications
+    EntityManager::get()->registerNotification( this, true );
+}
+
+void EnPlayerNameDisplay::postInitialize()
+{
+    // register entity in order to get updated per simulation step.
+    EntityManager::get()->registerUpdate( this, true );
+}
+
+void EnPlayerNameDisplay::updateEntity( float deltaTime )
+{
+    if ( _updateTimer > PLAYERNAME_UPDATE_PERIOD )
+    {
+        _updateTimer = 0.0f;
+        updateName();
+    }
+    else
+    {
+        _updateTimer += deltaTime;
+    }
+}
+
+void EnPlayerNameDisplay::updateName()
+{
+    // do a simple in-sight check for all remote clients
+    EnPlayer* p_player = dynamic_cast< EnPlayer* >( CTD::gameutils::PlayerUtils::get()->getLocalPlayer() );
+    if ( !p_player )
+        return;
+
+    EnCamera*   p_playercamera = p_player->getPlayerImplementation()->getPlayerCamera();
+    const osg::Vec3f& lpos = p_playercamera->getCameraPosition();
+    const osg::Quat&  lrot = p_playercamera->getLocalRotation();
+    
+    // calculate the player look direction
+    osg::Vec3f lookdir( 1.0f, 0.0f, 0.0f );
+    lookdir = lrot * lookdir;
+    
+    // a line between a remote and local player
+    osg::Vec3f  line;
+
+    std::vector< BaseEntity* >& remoteplayers = CTD::gameutils::PlayerUtils::get()->getRemotePlayers();
+    std::vector< BaseEntity* >::iterator p_beg = remoteplayers.begin(), p_end = remoteplayers.end();
+    for ( ; p_beg != p_end; p_beg++ )
+    {
+        line = ( *p_beg )->getPosition() - lpos;
+        line.normalize();
+        if ( ( line * lookdir ) > cos( osg::PI / 10.0f ) )
+        {
+            //! TODO
+            static int s_i = 0;
+            std::stringstream i;
+            i << s_i++;
+            CTD::log << CTD::Log::LogLevel( CTD::Log::L_DEBUG ) << i.str() << " player '"  << ( *p_beg )->getInstanceName() << "' is in fron" << std::endl;
+        }
+    }
+}
+
+} // namespace CTD
---
+++ trunk/ctd/src/gamecode/player/ctd_playernamedisp.h
@@ -0,0 +1,103 @@
+/****************************************************************
+ *  YAG2002 (http://yag2002.sourceforge.net)
+ *  Copyright (C) 2005-2007, A. Botorabi
+ *
+ *  This program is free software; you can redistribute it and/or 
+ *  modify it under the terms of the GNU Lesser General Public 
+ *  License version 2.1 as published by the Free Software Foundation.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public 
+ *  License along with this program; if not, write to the Free 
+ *  Software Foundation, Inc., 59 Temple Place, Suite 330, Boston, 
+ *  MA  02111-1307  USA
+ * 
+ ****************************************************************/
+
+/*###############################################################
+ # entity for displaying name of player in front
+ #
+ #   date of creation:  11/02/2005
+ #
+ #   author:            ali botorabi (boto) 
+ #      e-mail:         botorabi@gmx.net
+ #
+ ################################################################*/
+
+#ifndef _CTD_PLAYERNAMEDISP_H_
+#define _CTD_PLAYERNAMEDISP_H_
+
+#include <ctd_main.h>
+
+namespace CTD
+{
+
+//! Period for updating name
+#define PLAYERNAME_UPDATE_PERIOD        0.5f
+
+//! Entity name
+#define ENTITY_NAME_PLAYERNAMEDISP      "PlayerNameDisplay"
+
+//! Entity for displaying player name in front of local player
+class EnPlayerNameDisplay : public BaseEntity
+{
+    public:
+
+
+                                                    EnPlayerNameDisplay();
+
+        virtual                                     ~EnPlayerNameDisplay();
+
+
+        //! This entity does not need a transform node
+        const bool                                  isTransformable() const { return false; }
+
+        
+        //! Initializing function, this is called after all engine modules are initialized and a map is loaded.
+        void                                        initialize();
+
+       
+        //! Post-initializing function, this is called after all plugins' entities are initilized.
+        void                                        postInitialize();
+
+    protected:
+
+        //! Update entity
+        void                                        updateEntity( float deltaTime );
+
+        //! Override this method of BaseEntity to get notifications (from menu system)
+        void                                        handleNotification( const EntityNotification& notification );
+
+        //! Determine the name of player in front and update the name gui
+        void                                        updateName();
+
+        //! Relative display gui position screen [ 0..1, 0..1 ] ( only x and y coords are used )
+        osg::Vec3f                                  _position;
+
+        //! List of player entities
+        std::vector< BaseEntity* >                  _players;
+
+        //! Internal timer for updating the name
+        float                                       _updateTimer;
+};
+
+//! Entity type definition used for type registry
+class PlayerNameDisplayEntityFactory : public BaseEntityFactory
+{
+    public:
+                                                    PlayerNameDisplayEntityFactory() : 
+                                                     BaseEntityFactory( ENTITY_NAME_PLAYERNAMEDISP, BaseEntityFactory::Client )
+                                                    {}
+
+        virtual                                     ~PlayerNameDisplayEntityFactory() {}
+
+        Macro_CreateEntity( EnPlayerNameDisplay );
+};
+
+} // namespace CTD
+
+#endif // _CTD_PLAYERNAMEDISP_H_
