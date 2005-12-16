e1178c0c8b8707736460f84382a4efea4093b412
botorabi <botorabi@789ac5a4-3de6-0310-9700-cb8f6bc1df3f>
2005-12-16 19:56:02 +0000

    release 1.0.1
    - added support for scene file conversion in console cmd 'writeosgfile'
    
    - fixed a mem leak in level selection gui
    
    - added support for configuring the log level via .cfg file
    
    - changed fullscreen option in menu Settings/Display to checkbox (previousely it was a radio button)

---
+++ tags/VRC-v1.0.1/src/gamecode/tools/console/vrc_cmdconfigget.cpp
@@ -0,0 +1,77 @@
+/****************************************************************
+ *  YAG2002 (http://yag2002.sourceforge.net)
+ *  Copyright (C) 2005-2007, A. Botorabi
+ *
+ *  This program is free software; you can redistribute it and/or 
+ *  modify it under the terms of the GNU Lesser General Public 
+ *  License version 2.1 as published by the Free Software Foundation.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public 
+ *  License along with this program; if not, write to the Free 
+ *  Software Foundation, Inc., 59 Temple Place, Suite 330, Boston, 
+ *  MA  02111-1307  USA
+ * 
+ ****************************************************************/
+
+/*###############################################################
+ # console command for showing the current configuration
+ #
+ #   date of creation:  14/06/2005
+ #
+ #   author:            ali botorabi (boto) 
+ #      e-mail:         botorabi@gmx.net
+ #
+ ################################################################*/
+
+#include <vrc_main.h>
+#include "vrc_basecmd.h"
+#include "vrc_cmdconfigget.h"
+
+namespace vrc
+{
+
+//! Implement and register the command
+VRC_IMPL_CONSOLE_CMD( CmdConfigGet );
+
+
+CmdConfigGet::CmdConfigGet() :
+ BaseConsoleCommand( CMD_NAME_CONFIGGET )
+{
+    setUsage( CMD_USAGE_CONFIGGET );
+}
+
+CmdConfigGet::~CmdConfigGet()
+{
+}
+
+const std::string& CmdConfigGet::execute( const std::vector< std::string >& arguments )
+{
+    if ( arguments.size() != 1 )
+    {
+        _cmdResult += getUsage();
+        return _cmdResult;
+    }
+
+    std::vector< std::pair< std::string, std::string > > settings;
+    yaf3d::Configuration::get()->getConfigurationAsString( settings );
+    std::vector< std::pair< std::string, std::string > >::iterator p_beg = settings.begin(), p_end = settings.end();
+    for ( ; p_beg != p_end; ++p_beg )
+    {
+        if ( p_beg->first == arguments[ 0 ] )
+        {
+            _cmdResult += p_beg->first + " [" + p_beg->second + "]\n";
+            break;
+        }
+    }
+    if ( p_beg == p_end )
+        _cmdResult = "configuration setting '" + arguments[ 0 ] +  "' does not exist";
+
+    return _cmdResult;
+}
+
+} // namespace vrc
---
+++ tags/VRC-v1.0.1/src/gamecode/tools/console/vrc_cmdentitylist.cpp
@@ -0,0 +1,75 @@
+/****************************************************************
+ *  YAG2002 (http://yag2002.sourceforge.net)
+ *  Copyright (C) 2005-2007, A. Botorabi
+ *
+ *  This program is free software; you can redistribute it and/or 
+ *  modify it under the terms of the GNU Lesser General Public 
+ *  License version 2.1 as published by the Free Software Foundation.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public 
+ *  License along with this program; if not, write to the Free 
+ *  Software Foundation, Inc., 59 Temple Place, Suite 330, Boston, 
+ *  MA  02111-1307  USA
+ * 
+ ****************************************************************/
+
+/*###############################################################
+ # console command for getting an entity list
+ #
+ #   date of creation:  06/14/2005
+ #
+ #   author:            ali botorabi (boto) 
+ #      e-mail:         botorabi@gmx.net
+ #
+ ################################################################*/
+
+#include <vrc_main.h>
+#include "vrc_basecmd.h"
+#include "vrc_cmdentitylist.h"
+
+namespace vrc
+{
+
+//! Implement and register the command
+VRC_IMPL_CONSOLE_CMD( CmdEntityList );
+
+
+CmdEntityList::CmdEntityList() :
+ BaseConsoleCommand( CMD_NAME_ENTITYLIST )
+{
+    setUsage( CMD_USAGE_ENTITYLIST );
+}
+
+CmdEntityList::~CmdEntityList()
+{
+}
+
+const std::string& CmdEntityList::execute( const std::vector< std::string >& arguments )
+{
+    _cmdResult  = "";
+
+    std::string info;
+    std::string flags;
+    std::vector< yaf3d::BaseEntity* > entities;
+    yaf3d::EntityManager::get()->getAllEntities( entities );
+    std::vector< yaf3d::BaseEntity* >::iterator p_beg = entities.begin(), p_end = entities.end();
+    for ( ; p_beg != p_end; ++p_beg )
+    {
+        flags = "----";
+        if ( ( *p_beg )->isPersistent() ) flags[ 0 ] = 'P';
+        if ( ( *p_beg )->isTransformable() ) flags[ 1 ] = 'T';
+        if ( yaf3d::EntityManager::get()->isRegisteredUpdate( *p_beg ) ) flags[ 2 ] = 'U';
+        if ( yaf3d::EntityManager::get()->isRegisteredNotification( *p_beg ) ) flags[ 3 ] = 'N';
+
+        info += ( *p_beg )->getInstanceName() + "(" + ( *p_beg )->getTypeName() + ", " + flags + ")\n";
+    }
+    _cmdResult += info + "\n";
+    return _cmdResult;
+}
+
+} // namespace vrc
