f5a8b93c4aff869d544574153e32f321cc3c4253
botorabi <botorabi@789ac5a4-3de6-0310-9700-cb8f6bc1df3f>
2006-01-15 02:10:22 +0000

    updated network protocol version to 1.1.0

--- trunk/yag2002/src/framework/network.h
+++ trunk/yag2002/src/framework/network.h
@@ -54,7 +54,7 @@
 
 //! Network protocol version, use getProtocolVersionAsString to convert it to a string
 // Format: 0 x 00 - Current - Revision - Age
-#define YAF3D_NETWORK_PROT_VERSION      0x00010002
+#define YAF3D_NETWORK_PROT_VERSION      0x00010100
 //! Helper function for converting the protocol version to a string
 std::string getProtocolVersionAsString( unsigned int version );
 
