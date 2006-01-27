66cd7a25865c1bdef453ba1d076ca6b14141bce3
botorabi <botorabi@789ac5a4-3de6-0310-9700-cb8f6bc1df3f>
2006-01-27 00:46:42 +0000

    small fix (thanks to dtidrow)

--- trunk/yag2002/src/gamecode/player/chat/VRC/networkingRoles/_RO_PlayerChat.h
+++ trunk/yag2002/src/gamecode/player/chat/VRC/networkingRoles/_RO_PlayerChat.h
@@ -24,33 +24,33 @@ typedef struct _tChatData
 
 } tChatData;
 
-_RO_DEF_ALLOCATEFUNC(ImplChatNetworkingVRC);
+_RO_DEF_ALLOCATEFUNC(ImplChatNetworkingVRC)
 class _MAKE_BASE(ImplChatNetworkingVRC);
 class _RO_ImplChatNetworkingVRC : public RNReplicaNet::ReplicaObject
 {
-	_RO_STD_FRAMEWORK(ImplChatNetworkingVRC);
-	_RO_DO_DEFBLOCK_FUNCTION_VAR(RPC_RequestJoin);
+	_RO_STD_FRAMEWORK(ImplChatNetworkingVRC)
+	_RO_DO_DEFBLOCK_FUNCTION_VAR(RPC_RequestJoin)
 	void Call_RPC_RequestJoin(tChatData);
-	_RO_DO_DEFBLOCK_FUNCTION_VAR(RPC_ClientJoined);
+	_RO_DO_DEFBLOCK_FUNCTION_VAR(RPC_ClientJoined)
 	void Call_RPC_ClientJoined(tChatData);
-	_RO_DO_DEFBLOCK_FUNCTION_VAR(RPC_RequestLeave);
+	_RO_DO_DEFBLOCK_FUNCTION_VAR(RPC_RequestLeave)
 	void Call_RPC_RequestLeave(tChatData);
-	_RO_DO_DEFBLOCK_FUNCTION_VAR(RPC_ClientLeft);
+	_RO_DO_DEFBLOCK_FUNCTION_VAR(RPC_ClientLeft)
 	void Call_RPC_ClientLeft(tChatData);
-	_RO_DO_DEFBLOCK_FUNCTION_VAR(RPC_InitializeClient);
+	_RO_DO_DEFBLOCK_FUNCTION_VAR(RPC_InitializeClient)
 	void Call_RPC_InitializeClient(tChatData);
-	_RO_DO_DEFBLOCK_FUNCTION_VAR(RPC_RequestChangeNickname);
+	_RO_DO_DEFBLOCK_FUNCTION_VAR(RPC_RequestChangeNickname)
 	void Call_RPC_RequestChangeNickname(tChatData);
-	_RO_DO_DEFBLOCK_FUNCTION_VAR(RPC_ChangedNickname);
+	_RO_DO_DEFBLOCK_FUNCTION_VAR(RPC_ChangedNickname)
 	void Call_RPC_ChangedNickname(tChatData);
-	_RO_DO_DEFBLOCK_FUNCTION_VAR(RPC_RequestMemberList);
+	_RO_DO_DEFBLOCK_FUNCTION_VAR(RPC_RequestMemberList)
 	void Call_RPC_RequestMemberList(tChatData);
-	_RO_DO_DEFBLOCK_FUNCTION_VAR(RPC_RecvMemberList);
+	_RO_DO_DEFBLOCK_FUNCTION_VAR(RPC_RecvMemberList)
 	void Call_RPC_RecvMemberList(tChatData);
-	_RO_DO_DEFBLOCK_FUNCTION_VAR(RPC_PostChatText);
+	_RO_DO_DEFBLOCK_FUNCTION_VAR(RPC_PostChatText)
 	void Call_RPC_PostChatText(tChatMsg);
-	_RO_DO_DEFBLOCK_FUNCTION_VAR(RPC_RecvChatText);
+	_RO_DO_DEFBLOCK_FUNCTION_VAR(RPC_RecvChatText)
 	void Call_RPC_RecvChatText(tChatMsg);
-	_RO_DEF_REGISTERDATABLOCKS(ImplChatNetworkingVRC);
+	_RO_DEF_REGISTERDATABLOCKS(ImplChatNetworkingVRC)
 };
 #endif
