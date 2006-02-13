3acdfdecfaa0e297fd07d2432f7908047e367e37
botorabi <botorabi@789ac5a4-3de6-0310-9700-cb8f6bc1df3f>
2006-02-13 19:53:59 +0000

    extensions for voice chat

--- trunk/yag2002/src/gamecode/player/networkingRoles/_RO_PlayerNetworking.h
+++ trunk/yag2002/src/gamecode/player/networkingRoles/_RO_PlayerNetworking.h
@@ -1,24 +1,29 @@
-/* Auto-generated with RNROLCompiler V2.2.0.0 don't edit */
+/* Auto-generated with RNROLCompiler V2.3.0.0 don't edit */
 #ifndef ___RO_PlayerNetworking_h__
 #define ___RO_PlayerNetworking_h__
 #include "RNReplicaNet/Inc/ReplicaObject.h"
 #include "RNReplicaNet/Inc/Define_RO.h"
-_RO_DEF_ALLOCATEFUNC(PlayerNetworking)
 
 typedef struct _tInitializationData
 {
     float   _posX;
     float   _posY;
     float   _posZ;
-    float   _rotZ;   
+    float   _rotZ;
+    char    _ip[ 24 ];
 } tInitializationData;
 
+_RO_DEF_ALLOCATEFUNC(PlayerNetworking);
 class _MAKE_BASE(PlayerNetworking);
 class _RO_PlayerNetworking : public RNReplicaNet::ReplicaObject
 {
-	_RO_STD_FRAMEWORK(PlayerNetworking)
-	_RO_DO_DEFBLOCK_FUNCTION_VAR(RPC_Initialize)
+	_RO_STD_FRAMEWORK(PlayerNetworking);
+	_RO_DO_DEFBLOCK_FUNCTION_VAR(RPC_ServerGrantsAccess);
+	void Call_RPC_ServerGrantsAccess(tInitializationData);
+	_RO_DO_DEFBLOCK_FUNCTION_VAR(RPC_Initialize);
 	void Call_RPC_Initialize(tInitializationData);
-	_RO_DEF_REGISTERDATABLOCKS(PlayerNetworking)
+	_RO_DO_DEFBLOCK_FUNCTION_VAR(RPC_EnableVoiceChat);
+	void Call_RPC_EnableVoiceChat(bool);
+	_RO_DEF_REGISTERDATABLOCKS(PlayerNetworking);
 };
 #endif
