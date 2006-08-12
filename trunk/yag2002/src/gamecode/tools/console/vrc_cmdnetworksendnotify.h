94618f25ec50822731fddc2f4c9378d9d1c92990
botorabi <botorabi@789ac5a4-3de6-0310-9700-cb8f6bc1df3f>
2006-08-12 16:53:56 +0000

    corrected a typo in help string

--- trunk/yag2002/src/gamecode/tools/console/vrc_cmdnetworksendnotify.h
+++ trunk/yag2002/src/gamecode/tools/console/vrc_cmdnetworksendnotify.h
@@ -2,8 +2,8 @@
  *  YAG2002 (http://yag2002.sourceforge.net)
  *  Copyright (C) 2005-2006, A. Botorabi
  *
- *  This program is free software; you can redistribute it and/or 
- *  modify it under the terms of the GNU Lesser General Public 
+ *  This program is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public
  *  License version 2.1 as published by the Free Software Foundation.
  *
  *  This program is distributed in the hope that it will be useful,
@@ -11,20 +11,20 @@
  *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  *  GNU General Public License for more details.
  *
- *  You should have received a copy of the GNU Lesser General Public 
- *  License along with this program; if not, write to the Free 
- *  Software Foundation, Inc., 59 Temple Place, Suite 330, Boston, 
+ *  You should have received a copy of the GNU Lesser General Public
+ *  License along with this program; if not, write to the Free
+ *  Software Foundation, Inc., 59 Temple Place, Suite 330, Boston,
  *  MA  02111-1307  USA
- * 
+ *
  ****************************************************************/
 
 /*###############################################################
- # console command for sending network notification, useful only 
+ # console command for sending network notification, useful only
  #  on server.
  #
  #   date of creation:  08/11/2006
  #
- #   author:            ali botorabi (boto) 
+ #   author:            ali botorabi (boto)
  #      e-mail:         botorabi@gmx.net
  #
  ################################################################*/
@@ -41,7 +41,7 @@ namespace vrc
 #define CMD_USAGE_NETWORKSENDNOTIFY   "send a notification to all connected clients\n"\
                                       " if a destruction timeout is given then the notification messagebox\n"\
                                       " automatically disappears after that timeout, -1 means no auto-destruction,\n"\
-                                      " 0 of missing destuction timeout in argument causes taking the default timeout.\n"\
+                                      " 0 or missing destuction timeout in argument causes taking the default timeout.\n"\
                                       "\n"\
                                       "use: network.send.notification < title > < text > [ destruction timeout in sec: > 0, -1, or 0 ]"
 
