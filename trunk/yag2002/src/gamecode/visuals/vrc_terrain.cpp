870d6b3e84ab6b900cbf31cf4af2141d9ee53faa
botorabi <botorabi@789ac5a4-3de6-0310-9700-cb8f6bc1df3f>
2007-08-02 23:01:01 +0000

    new: terrain entity; work in progress

---
+++ trunk/yag2002/src/gamecode/visuals/vrc_terrain.cpp
@@ -0,0 +1,154 @@
+/****************************************************************
+ *  YAG2002 (http://yag2002.sourceforge.net)
+ *  Copyright (C) 2005-2006, A. Botorabi
+ *
+ *  This program is free software; you can redistribute it and/or 
+ *  modify it under the terms of the GNU Lesser General Public 
+ *  License version 2.1 as published by the Free Software Foundation.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public 
+ *  License along with this program; if not, write to the Free 
+ *  Software Foundation, Inc., 59 Temple Place, Suite 330, Boston, 
+ *  MA  02111-1307  USA
+ * 
+ ****************************************************************/
+
+/*###############################################################
+ # entity terrain
+ #
+ #   date of creation:  07/27/2007
+ #
+ #   author:            ali botorabi (boto) 
+ #      e-mail:         botorabi@gmx.net
+ #
+ ################################################################*/
+
+#include <vrc_main.h>
+#include "vrc_terrain.h"
+#include <terrainmanager.h>
+
+namespace vrc
+{
+
+//! Implement and register the terrain entity factory
+YAF3D_IMPL_ENTITYFACTORY( TerrainEntityFactory )
+
+
+EnTerrain::EnTerrain() :
+_enable( true ),
+_scale( osg::Vec3f( 100.0f, 100.0f, 100.0f ) )
+{
+    // register entity attributes
+    getAttributeManager().addAttribute( "enable"           , _enable           );
+    getAttributeManager().addAttribute( "scale"            , _scale            );
+    getAttributeManager().addAttribute( "heightmap"        , _fileHeightmap    );
+    getAttributeManager().addAttribute( "basemap"          , _fileBasemap      );
+    getAttributeManager().addAttribute( "basemapRepeat"    , _basemapRepeat    );
+    getAttributeManager().addAttribute( "detailmap0"       , _fileDetailmap0   );
+    getAttributeManager().addAttribute( "detailmap0Repeat" , _detailmap0Repeat );
+    getAttributeManager().addAttribute( "detailmap1"       , _fileDetailmap1   );
+    getAttributeManager().addAttribute( "detailmap1Repeat" , _detailmap1Repeat );
+}
+
+EnTerrain::~EnTerrain()
+{
+}
+
+void EnTerrain::handleNotification( const yaf3d::EntityNotification& notification )
+{
+    // handle notifications
+    switch( notification.getId() )
+    {
+        case YAF3D_NOTIFY_MENU_ENTER:
+
+            if ( _enable )
+            {
+                removeFromTransformationNode( _p_terrainGrp.get() );
+            }
+            break;
+
+        case YAF3D_NOTIFY_MENU_LEAVE:
+
+            if ( _enable )
+            {
+                addToTransformationNode( _p_terrainGrp.get() );
+            }
+            break;
+
+        // re-create the skybox whenever an attribute changed
+        case YAF3D_NOTIFY_ENTITY_ATTRIBUTE_CHANGED:
+            removeFromTransformationNode( _p_terrainGrp.get() );
+            _p_terrainGrp = setup();
+            if ( _enable )
+                addToTransformationNode( _p_terrainGrp.get() );
+
+        // if used in menu then this entity is persisten, so we have to trigger its deletion on shutdown
+        case YAF3D_NOTIFY_SHUTDOWN:
+
+            break;
+
+        default:
+            ;
+    }
+}
+
+void EnTerrain::initialize()
+{
+    // register entity in order to get notifications   
+    yaf3d::EntityManager::get()->registerNotification( this, true );   
+
+    // setup the terrain
+    _p_terrainGrp = setup();
+
+//! TODO remove this later! we need the coupling to menu system!
+addToTransformationNode( _p_terrainGrp.get() );
+}
+
+osg::ref_ptr< osg::Group > EnTerrain::setup()
+{
+    osg::ref_ptr< osg::Group > group;
+
+    yaf3d::TerrainConfig conf;
+    conf._scale             = _scale;
+    conf._fileHeightmap     = _fileHeightmap;
+    conf._fileBasemap       = _fileBasemap;
+    conf._basemapRepeat     = _basemapRepeat;
+    conf._fileDetailmap0    = _fileDetailmap0;
+    conf._detailmap0Repeat  = _detailmap0Repeat;
+    conf._fileDetailmap1    = _fileDetailmap0;
+    conf._detailmap1Repeat  = _detailmap0Repeat;
+
+    try
+    {
+        group = yaf3d::TerrainManager::get()->setup( conf );
+    }
+    catch( const yaf3d::TerrainException& e )
+    {
+        log_error << "cannot create terrain entity." << std::endl;
+        log_error << " reason: " << e.what() << std::endl;
+    }
+
+    return group;
+}
+
+void EnTerrain::enable( bool en )
+{
+    if ( en == _enable )
+        return;
+
+    if ( !_p_terrainGrp.valid() )
+        return;
+
+    _enable = en;
+    if ( en )
+        addToTransformationNode( _p_terrainGrp.get() );
+    else
+        removeFromTransformationNode( _p_terrainGrp.get() );
+}
+
+} // namespace vrc
---
+++ trunk/yag2002/src/gamecode/visuals/vrc_terrain.h
@@ -0,0 +1,113 @@
+/****************************************************************
+ *  YAG2002 (http://yag2002.sourceforge.net)
+ *  Copyright (C) 2005-2006, A. Botorabi
+ *
+ *  This program is free software; you can redistribute it and/or 
+ *  modify it under the terms of the GNU Lesser General Public 
+ *  License version 2.1 as published by the Free Software Foundation.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public 
+ *  License along with this program; if not, write to the Free 
+ *  Software Foundation, Inc., 59 Temple Place, Suite 330, Boston, 
+ *  MA  02111-1307  USA
+ * 
+ ****************************************************************/
+
+/*###############################################################
+ # entity terrain
+ #
+ #   date of creation:  07/27/2007
+ #
+ #   author:            ali botorabi (boto) 
+ #      e-mail:         botorabi@gmx.net
+ #
+ ################################################################*/
+
+#ifndef _VRC_TERRAIN_H_
+#define _VRC_TERRAIN_H_
+
+#include <vrc_main.h>
+#include <vrc_gameutils.h>
+
+namespace vrc
+{
+
+#define ENTITY_NAME_TERRAIN    "Terrain"
+
+class EnTerrain :  public yaf3d::BaseEntity
+{
+    public:
+                                                    EnTerrain();
+
+        virtual                                     ~EnTerrain();
+        
+        //! Initializing function, this is called after all engine modules are initialized and a map is loaded.
+        void                                        initialize();
+
+        //! Enable / disable terrain rendering
+        void                                        enable( bool en );
+
+    protected:
+
+        //! This entity is persistent so it has to trigger its destruction on shutdown ifself.
+        void                                        handleNotification( const yaf3d::EntityNotification& notification );
+
+        //! Setup the terrain
+        osg::ref_ptr< osg::Group >                  setup();
+
+
+        // Entity parameters
+        //------------------
+
+        //! Scaling the height and X/Y
+        osg::Vec3f                                  _scale;
+
+        //! Heightmap
+        std::string                                 _fileHeightmap;
+
+        //! Basemap
+        std::string                                 _fileBasemap;
+
+        //! Repeat factor in X/Y direction for basemap
+        osg::Vec2f                                  _basemapRepeat;
+
+        //! Detail map 0 file
+        std::string                                 _fileDetailmap0;
+
+        //! Repeat factor in X/Y direction for detailmap 0
+        osg::Vec2f                                  _detailmap0Repeat;
+
+        //! Detail map 1 file
+        std::string                                 _fileDetailmap1;
+
+        //! Repeat factor in X/Y direction for detailmap 1
+        osg::Vec2f                                  _detailmap1Repeat;
+
+        //------------------
+           
+        osg::ref_ptr< osg::Group >                  _p_terrainGrp;
+
+        bool                                        _enable;
+};
+
+//! Entity type definition used for type registry
+class TerrainEntityFactory : public yaf3d::BaseEntityFactory
+{
+    public:
+                                                    TerrainEntityFactory() : 
+                                                     yaf3d::BaseEntityFactory( ENTITY_NAME_TERRAIN, yaf3d::BaseEntityFactory::Standalone | yaf3d::BaseEntityFactory::Client )
+                                                    {}
+
+        virtual                                     ~TerrainEntityFactory() {}
+
+        Macro_CreateEntity( EnTerrain );
+};
+
+} // namespace vrc
+
+#endif // _VRC_TERRAIN_H_
