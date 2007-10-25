c6ae5de1028bc09490f6c44c41ff83b6491a83ba
botorabi <botorabi@789ac5a4-3de6-0310-9700-cb8f6bc1df3f>
2007-10-25 21:59:24 +0000

    batch file for building the role files in win32

---
+++ tags/branch-storage/src/gamecode/player/networkingRoles/build_roles.bat
@@ -0,0 +1,6 @@
+@echo compiling _RO_PlayerNetworking.rol
+@echo ----------------------------------
+
+%REPLICANET_INC_DIR%\..\RNROLCompiler\bin\rnrolcompiler.exe _RO_PlayerNetworking.rol _RO_PlayerNetworking.cpp _RO_PlayerNetworking.h
+
+pause
