c0c23d64fc2fe5c96fe2ec35b204a6d9a22bca98
botorabi <botorabi@789ac5a4-3de6-0310-9700-cb8f6bc1df3f>
2007-11-01 23:13:45 +0000

    added networking to interactive objects

---
+++ trunk/yag2002/src/gamecode/objects/networkingRoles/_RO_ObjectNetworking.h
@@ -0,0 +1,45 @@
+/* Auto-generated with RNROLCompiler V2.7.0.0 don't edit */
+#ifndef ___RO_ObjectNetworking_h__
+#define ___RO_ObjectNetworking_h__
+#include "RNReplicaNet/Inc/ReplicaObject.h"
+#include "RNReplicaNet/Inc/Define_RO.h"
+namespace vrc
+{
+
+typedef struct _tObjectData
+{
+    //! User ID of picking user
+    unsigned int    _userID;
+
+    //! Used for object respawn
+    unsigned int    _objectID;
+
+    float           _posX;
+    float           _posY;
+    float           _posZ;
+    float           _rotZ;
+
+    //! Object parameters depending on object type
+    unsigned int    _param1;
+    unsigned int    _param2;
+    unsigned int    _param3;
+    unsigned int    _param4;
+
+} tObjectData;
+
+
+_RO_DEF_ALLOCATEFUNC(ObjectNetworking)
+class _MAKE_BASE(ObjectNetworking);
+class _RO_ObjectNetworking : public RNReplicaNet::ReplicaObject
+{
+	_RO_STD_FRAMEWORK(ObjectNetworking)
+	_RO_DO_DEFBLOCK_FUNCTION_VAR(RPC_Respawn)
+	void Call_RPC_Respawn(tObjectData);
+	typedef void(tDBFV_RPC_Respawn)(tObjectData);
+	_RO_DO_DEFBLOCK_FUNCTION_VAR(RPC_Use)
+	void Call_RPC_Use(tObjectData);
+	typedef void(tDBFV_RPC_Use)(tObjectData);
+	_RO_DEF_REGISTERDATABLOCKS(ObjectNetworking)
+};
+} // namespace vrc
+#endif
---
+++ trunk/yag2002/src/gamecode/objects/networkingRoles/build_roles.bat
@@ -0,0 +1,6 @@
+@echo compiling _RO_ObjectNetworking.rol
+@echo ----------------------------------
+
+%REPLICANET_INC_DIR%\..\RNROLCompiler\bin\rnrolcompiler.exe _RO_ObjectNetworking.rol _RO_ObjectNetworking.cpp _RO_ObjectNetworking.h
+
+pause
