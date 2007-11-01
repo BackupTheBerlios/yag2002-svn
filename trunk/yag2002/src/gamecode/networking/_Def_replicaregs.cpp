1e4cee6d4d3d7c711999adfb482b82f3cba54a61
botorabi <botorabi@789ac5a4-3de6-0310-9700-cb8f6bc1df3f>
2007-11-01 23:15:19 +0000

    added interactive object to network types

--- trunk/yag2002/src/gamecode/networking/_Def_replicaregs.cpp
+++ trunk/yag2002/src/gamecode/networking/_Def_replicaregs.cpp
@@ -7,4 +7,5 @@ _RO_DEF_DO_PREDEF(ImplChatNetworkingVRC);
 _RO_DEF_DO_PREDEF(PlayerNetworking);
 _RO_DEF_DO_PREDEF(NetworkNotificationNetworking);
 _RO_DEF_DO_PREDEF(StorageNetworking);
+_RO_DEF_DO_PREDEF(ObjectNetworking);
 _RO_DO_PREREGISTER_FUNC_END()
